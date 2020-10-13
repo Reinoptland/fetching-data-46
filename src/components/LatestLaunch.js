ac4cdc9d01edab44f80891c74f708a53fb515e28
Reinoptland <reinoptland@gmail.com>
2020-10-13 16:16:22 +0200

    Example with dependency array

--- src/components/LatestLaunch.js
+++ src/components/LatestLaunch.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export default function LatestLaunch() {
   const [launchData, setLaunchData] = useState(null);
+  const [launchId, setLaunchId] = useState(null);
 
   // useEffect
   // 1st argument: callback
@@ -18,10 +19,17 @@ export default function LatestLaunch() {
       console.log(response.data);
       // set the state
       setLaunchData(response.data);
+      // set to latest launch Id
+      setLaunchId(response.data.id);
     }
 
     getLaunchData();
   }, []);
+
+  useEffect(() => {
+    console.log("Launch id has changed:", launchId);
+    console.log("fetch more data");
+  }, [launchId]);
 
   if (launchData === null) {
     return <h1>Loading</h1>;
@@ -30,11 +38,17 @@ export default function LatestLaunch() {
   const message =
     launchData.success === true ? "Elon did a thing" : "Elon did a stupid";
 
+  function decrementLaunchId() {
+    setLaunchId(launchId - 1);
+  }
+
   return (
     <div>
       <h1>{launchData.name}</h1>
       <img src={launchData.links.patch.small} />
       <h4>{message}</h4>
+      <button onClick={decrementLaunchId}>Previous launch</button>
+      Viewing launchId: {launchId}
     </div>
   );
 }
